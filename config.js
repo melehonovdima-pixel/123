ffaa6db94444db8221231a3a1927588c48319f35
melehonovdima-pixel <melehonovdima@gmail.com>
2026-02-25 06:58:26 +0300

    Update API_CONFIG with new BASE_URL

--- config.js
+++ config.js
@@ -1,5 +1,6 @@
 const API_CONFIG = {
-    BASE_URL: 'https://proj.moj-proj.netcraze.pro',
+    BASE_URL: 'https://resedaceous-stephan-addible.ngrok-free.dev',
     TOKEN_KEY: 'auth_token',
     USER_KEY: 'current_user'
+
 };
